aad2702ed4ef791d53ffd3f2859abf3df0b7649c
Riversleaf <sergo.joeleht@tktk.ee>
2023-01-27 18:25:20 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is a comment
+
+//rakendus küsib kasutajal sisetada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter Your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
